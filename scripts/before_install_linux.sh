662274e106f155c025e5d4e108ce1b6fe1d2f309
Mani Soundararajan <root@msound.net>
2018-02-25 00:51:17 -0500

    Migrating from godep to dep

--- scripts/before_install_linux.sh
+++ scripts/before_install_linux.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y build-essential cmake
-BUILD_DIR=`pwd`
-cd /tmp
-rm -f v0.25.1.tar.gz
-rm -rf libgit2-0.25.1/
-wget https://github.com/libgit2/libgit2/archive/v0.25.1.tar.gz
-tar -xzf v0.25.1.tar.gz
-cd libgit2-0.25.1/
-mkdir build && cd build
-cmake ..
-sudo cmake --build . --target install
-cd $BUILD_DIR
+# sudo apt-get update
+# sudo apt-get install -y build-essential cmake
+# BUILD_DIR=`pwd`
+# cd /tmp
+# rm -f v0.25.1.tar.gz
+# rm -rf libgit2-0.25.1/
+# wget https://github.com/libgit2/libgit2/archive/v0.25.1.tar.gz
+# tar -xzf v0.25.1.tar.gz
+# cd libgit2-0.25.1/
+# mkdir build && cd build
+# cmake ..
+# sudo cmake --build . --target install
+# cd $BUILD_DIR
+cd vendor/gopkg.in/libgit2/git2go.v25/
+git submodule update --init
+make install-static
+cd ../../../../
