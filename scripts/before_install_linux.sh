49b9bf695a13bb92233dc9e0abf9984de3f2c216
Mani Soundararajan <root@msound.net>
2018-02-25 21:55:19 -0500

    Fixed build path

--- scripts/before_install_linux.sh
+++ scripts/before_install_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y build-essential cmake
+sudo apt-get install -y build-essential cmake libpthread-stubs0-dev libssl-dev libssh-dev
 BUILD_DIR=`pwd`
 cd /tmp
 rm -f v0.25.1.tar.gz
@@ -8,14 +8,12 @@ rm -rf libgit2-0.25.1/
 wget https://github.com/libgit2/libgit2/archive/v0.25.1.tar.gz
 tar -xzf v0.25.1.tar.gz
 cd libgit2-0.25.1/
-mkdir -p install/lib
 mkdir -p build && cd build
 cmake -DTHREADSAFE=ON \
       -DBUILD_CLAR=OFF \
       -DBUILD_SHARED_LIBS=OFF \
       -DCMAKE_C_FLAGS=-fPIC \
       -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
-      -DCMAKE_INSTALL_PREFIX=../install \
       ..
-sudo cmake --build
+sudo cmake --build . --target install
 cd $BUILD_DIR
