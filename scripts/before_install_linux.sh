2c0b4fa11434ccf01313b9b018ff6ee41f508d47
Mani Soundararajan <root@msound.net>
2018-02-25 21:50:06 -0500

    Static linking of libgit2

--- scripts/before_install_linux.sh
+++ scripts/before_install_linux.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-# sudo apt-get update
-# sudo apt-get install -y build-essential cmake
-# BUILD_DIR=`pwd`
-# cd /tmp
-# rm -f v0.25.1.tar.gz
-# rm -rf libgit2-0.25.1/
-# wget https://github.com/libgit2/libgit2/archive/v0.25.1.tar.gz
-# tar -xzf v0.25.1.tar.gz
-# cd libgit2-0.25.1/
-# mkdir build && cd build
-# cmake ..
-# sudo cmake --build . --target install
-# cd $BUILD_DIR
-cd vendor/gopkg.in/libgit2/git2go.v25/
-git submodule update --init
-make install-static
-cd ../../../../
+sudo apt-get update
+sudo apt-get install -y build-essential cmake
+BUILD_DIR=`pwd`
+cd /tmp
+rm -f v0.25.1.tar.gz
+rm -rf libgit2-0.25.1/
+wget https://github.com/libgit2/libgit2/archive/v0.25.1.tar.gz
+tar -xzf v0.25.1.tar.gz
+cd libgit2-0.25.1/
+mkdir -p install/lib
+mkdir -p build && cd build
+cmake -DTHREADSAFE=ON \
+      -DBUILD_CLAR=OFF \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_C_FLAGS=-fPIC \
+      -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
+      -DCMAKE_INSTALL_PREFIX=../install \
+      ..
+sudo cmake --build
+cd $BUILD_DIR
