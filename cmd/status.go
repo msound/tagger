e590dd98fea2eaf0b65264a7f2ba81b6fb841419
Mani Soundararajan <root@msound.net>
2018-11-02 15:50:33 -0400

    Added subcommand status

---
+++ cmd/status.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// statusCmd represents the status command
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "tagger status displays last version",
+	Long: `tagger status displays last version.
+It also displays how many commits have been made since the last tag.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("status called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(statusCmd)
+}
