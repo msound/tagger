74e7a301fecc35402416923c2ae78f57e807b84d
Mani Soundararajan <root@msound.net>
2018-11-02 15:35:59 -0400

    Added root cmd using cobra

---
+++ cmd/root.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "tagger",
+	Short: "Tagger helps bump version numbers in git projects",
+	Long: `Tagger helps developers to bump version numbers in git projects.
+Tagger also generates changelog based on merge commits.
+Additionally, Tagger creates annotated tags with changelog information.
+`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	// Search config in current directory with name ".tagger" (without extension).
+	viper.AddConfigPath(".")
+	viper.SetConfigName(".tagger")
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/msound/tagger/cmd"
+
+func main() {
+	cmd.Execute()
+}
