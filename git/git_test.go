7f5a620b5a45f77d5776565b14977750d852d353
Mani Soundararajan <root@msound.net>
2019-10-16 01:15:25 -0400

    Cleaning up git_test.go

--- git/git_test.go
+++ git/git_test.go
@@ -13,44 +13,17 @@ import (
 )
 
 func TestTagExists(t *testing.T) {
-	// Create a temp directory.
-	dirname := filepath.Join(os.TempDir(), randomString())
-	file := "README.md"
-	filename := filepath.Join(dirname, file)
+	// Create an empty repo.
+	dirname, err := makeEmptyRepo()
+	assert.Equal(t, nil, err, "Make empty repo")
 	t.Logf("Directory is: %s", dirname)
-	err := os.Mkdir(dirname, 0777)
-	assert.Equal(t, nil, err, "Make directory")
 
-	// Create a temp file.
-	f, err := os.Create(filename)
-	assert.Equal(t, nil, err, "Create README.md file")
-	_, err = f.Write([]byte("test\n"))
-	assert.Equal(t, nil, err, "Write to README.md file")
-	err = f.Close()
-	assert.Equal(t, nil, err, "Close README.md file")
-
-	// Initialize git in the temp directory.
-	cmd1 := exec.Command("git", "init")
-	cmd1.Dir = dirname
-	err = cmd1.Run()
-	assert.Equal(t, nil, err, "Git init")
-
-	// Add README.md to git staging area.
-	cmd2 := exec.Command("git", "add", file)
-	cmd2.Dir = dirname
-	err = cmd2.Run()
-	assert.Equal(t, nil, err, "Git add")
-
-	// Commit the file.
-	cmd3 := exec.Command("git", "commit", "-m", "Initial commit")
-	cmd3.Dir = dirname
-	err = cmd3.Run()
-	assert.Equal(t, nil, err, "Git commit")
+	// Create and commit README.md.
+	err = commitFile(dirname, "README.md", "test\n", "Initial commit")
+	assert.Equal(t, nil, err, "Create and commit README.md file")
 
 	// Create a tag.
-	cmd4 := exec.Command("git", "tag", "1.0.0")
-	cmd4.Dir = dirname
-	err = cmd4.Run()
+	err = createTag(dirname, "1.0.0")
 	assert.Equal(t, nil, err, "Git tag")
 
 	// Call the function to be tested.
@@ -66,10 +39,9 @@ func TestTagExists(t *testing.T) {
 
 func TestTagExistsNotInARepo(t *testing.T) {
 	// Create a temp directory.
-	dirname := filepath.Join(os.TempDir(), randomString())
-	t.Logf("Directory is: %s", dirname)
-	err := os.Mkdir(dirname, 0777)
+	dirname, err := makeTempDirectory()
 	assert.Equal(t, nil, err, "Make directory")
+	t.Logf("Directory is: %s", dirname)
 
 	// Call the function to be tested.
 	c := git.Client{dirname}
@@ -83,39 +55,14 @@ func TestTagExistsNotInARepo(t *testing.T) {
 }
 
 func TestTagExistsWithoutTag(t *testing.T) {
-	// Create a temp directory.
-	dirname := filepath.Join(os.TempDir(), randomString())
-	file := "README.md"
-	filename := filepath.Join(dirname, file)
+	// Create am empty repo.
+	dirname, err := makeEmptyRepo()
+	assert.Equal(t, nil, err, "Make empty repo")
 	t.Logf("Directory is: %s", dirname)
-	err := os.Mkdir(dirname, 0777)
-	assert.Equal(t, nil, err, "Make directory")
 
-	// Create a temp file.
-	f, err := os.Create(filename)
-	assert.Equal(t, nil, err, "Create README.md file")
-	_, err = f.Write([]byte("test\n"))
-	assert.Equal(t, nil, err, "Write to README.md file")
-	err = f.Close()
-	assert.Equal(t, nil, err, "Close README.md file")
-
-	// Initialize git in the temp directory.
-	cmd1 := exec.Command("git", "init")
-	cmd1.Dir = dirname
-	err = cmd1.Run()
-	assert.Equal(t, nil, err, "Git init")
-
-	// Add README.md to git staging area.
-	cmd2 := exec.Command("git", "add", file)
-	cmd2.Dir = dirname
-	err = cmd2.Run()
-	assert.Equal(t, nil, err, "Git add")
-
-	// Commit the file.
-	cmd3 := exec.Command("git", "commit", "-m", "Initial commit")
-	cmd3.Dir = dirname
-	err = cmd3.Run()
-	assert.Equal(t, nil, err, "Git commit")
+	// Create and commit README.md.
+	err = commitFile(dirname, "README.md", "test\n", "Initial commit")
+	assert.Equal(t, nil, err, "Create and commit README.md file")
 
 	// Call the function to be tested.
 	c := git.Client{dirname}
@@ -129,67 +76,22 @@ func TestTagExistsWithoutTag(t *testing.T) {
 }
 
 func TestChangelog(t *testing.T) {
-	// Create a temp directory.
-	dirname := filepath.Join(os.TempDir(), randomString())
-	file := "README.md"
-	filename := filepath.Join(dirname, file)
+	// Create an empty repo.
+	dirname, err := makeEmptyRepo()
+	assert.Equal(t, nil, err, "Make empty repo")
 	t.Logf("Directory is: %s", dirname)
-	err := os.Mkdir(dirname, 0777)
-	assert.Equal(t, nil, err, "Make directory")
 
-	// Create a temp file.
-	f, err := os.Create(filename)
-	assert.Equal(t, nil, err, "Create README.md file")
-	_, err = f.Write([]byte("test\n"))
-	assert.Equal(t, nil, err, "Write to README.md file")
-	err = f.Close()
-	assert.Equal(t, nil, err, "Close README.md file")
-
-	// Initialize git in the temp directory.
-	cmd1 := exec.Command("git", "init")
-	cmd1.Dir = dirname
-	err = cmd1.Run()
-	assert.Equal(t, nil, err, "Git init")
-
-	// Add README.md to git staging area.
-	cmd2 := exec.Command("git", "add", file)
-	cmd2.Dir = dirname
-	err = cmd2.Run()
-	assert.Equal(t, nil, err, "Git add README.md")
-
-	// Commit the file.
-	cmd3 := exec.Command("git", "commit", "-m", "Initial commit")
-	cmd3.Dir = dirname
-	err = cmd3.Run()
-	assert.Equal(t, nil, err, "Git commit")
+	// Create and commit README.md.
+	err = commitFile(dirname, "README.md", "test\n", "Initial commit")
+	assert.Equal(t, nil, err, "Create and commit README.md file")
 
 	// Create a tag.
-	cmd4 := exec.Command("git", "tag", "-a", "1.0.0", "-m", "Tagging")
-	cmd4.Dir = dirname
-	err = cmd4.Run()
+	err = createAnnotatedTag(dirname, "1.0.0", "Tagging")
 	assert.Equal(t, nil, err, "Git tag")
 
-	// Create another file.
-	file2 := "SECOND.md"
-	filename2 := filepath.Join(dirname, file2)
-	f2, err := os.Create(filename2)
-	assert.Equal(t, nil, err, "Create SECOND.md file")
-	_, err = f2.Write([]byte("test\n"))
-	assert.Equal(t, nil, err, "Write to SECOND.md file")
-	err = f2.Close()
-	assert.Equal(t, nil, err, "Close SECOND.md file")
-
-	// Add README.md to git staging area.
-	cmd5 := exec.Command("git", "add", file2)
-	cmd5.Dir = dirname
-	err = cmd5.Run()
-	assert.Equal(t, nil, err, "Git add SECOND.md")
-
-	// Commit the file.
-	cmd6 := exec.Command("git", "commit", "-m", "Merge pull request #2 from dev/branch\n\nAdded SECOND.md")
-	cmd6.Dir = dirname
-	err = cmd6.Run()
-	assert.Equal(t, nil, err, "Git commit")
+	// Create and commit another file.
+	err = commitFile(dirname, "SECOND.md", "test\n", "Merge pull request #2 from dev/branch\n\nAdded SECOND.md")
+	assert.Equal(t, nil, err, "Create and commit SECOND.md file")
 
 	// Call the function to be tested.
 	c := git.Client{dirname}
@@ -197,6 +99,69 @@ func TestChangelog(t *testing.T) {
 	assert.Nil(t, err, "Getting Changelog")
 	assert.Equal(t, 1, len(result))
 	assert.Equal(t, "#2 Added SECOND.md", result[0])
+
+	// Teardown the git directory.
+	err = os.RemoveAll(dirname)
+	assert.Equal(t, nil, err, "Remove directory")
+}
+
+func TestChangelogNotInARepo(t *testing.T) {
+	// Create a temp directory.
+	dirname, err := makeTempDirectory()
+	assert.Equal(t, nil, err, "Make directory")
+	t.Logf("Directory is: %s", dirname)
+
+	// Call the function to be tested.
+	c := git.Client{dirname}
+	result, err := c.Changelog("1.0.0")
+	assert.NotEqual(t, nil, err, "Due to non existant repo, err should not be nil")
+	assert.Equal(t, 0, len(result), "Changelog should be empty as there is no repo")
+
+	// Teardown the git directory.
+	err = os.RemoveAll(dirname)
+	assert.Equal(t, nil, err, "Remove directory")
+}
+
+func TestChangelogWithoutTag(t *testing.T) {
+	// Create am empty repo.
+	dirname, err := makeEmptyRepo()
+	assert.Equal(t, nil, err, "Make empty repo")
+	t.Logf("Directory is: %s", dirname)
+
+	// Create and commit README.md.
+	err = commitFile(dirname, "README.md", "test\n", "Initial commit")
+	assert.Equal(t, nil, err, "Create and commit README.md file")
+
+	// Call the function to be tested.
+	c := git.Client{dirname}
+	result, err := c.Changelog("1.0.0")
+	assert.NotEqual(t, nil, err, "Due to non existant tag, err should not be nil")
+	assert.Equal(t, 0, len(result), "Changelog should be empty as there is no tag")
+
+	// Teardown the git directory.
+	err = os.RemoveAll(dirname)
+	assert.Equal(t, nil, err, "Remove directory")
+}
+
+func TestChangelogWithLightweightTag(t *testing.T) {
+	// Create an empty repo.
+	dirname, err := makeEmptyRepo()
+	assert.Equal(t, nil, err, "Make empty repo")
+	t.Logf("Directory is: %s", dirname)
+
+	// Create and commit README.md.
+	err = commitFile(dirname, "README.md", "test\n", "Initial commit")
+	assert.Equal(t, nil, err, "Create and commit README.md file")
+
+	// Create a tag.
+	err = createTag(dirname, "1.0.0")
+	assert.Equal(t, nil, err, "Git tag")
+
+	// Call the function to be tested.
+	c := git.Client{dirname}
+	result, err := c.Changelog("1.0.0")
+	assert.NotNil(t, err, "Due to lightweight tag, err should not be nil")
+	assert.Equal(t, 0, len(result), "Changelog shoud be empty as there is no annotated tag")
 
 	// Teardown the git directory.
 	err = os.RemoveAll(dirname)
@@ -212,4 +177,83 @@ func randomString() string {
 	}
 
 	return value
+}
+
+func makeTempDirectory() (string, error) {
+	dirname := filepath.Join(os.TempDir(), randomString())
+	err := os.Mkdir(dirname, 0777)
+	return dirname, err
+}
+
+func makeEmptyRepo() (string, error) {
+	// Create a temp directory.
+	dirname, err := makeTempDirectory()
+	if err != nil {
+		return dirname, err
+	}
+
+	// Initialize git in the temp directory.
+	cmd1 := exec.Command("git", "init")
+	cmd1.Dir = dirname
+	err = cmd1.Run()
+
+	return dirname, err
+}
+
+func makeFile(dirname string, filename string, contents string) error {
+	file := filepath.Join(dirname, filename)
+	f, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	_, err = f.Write([]byte(contents))
+	if err != nil {
+		return err
+	}
+
+	err = f.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func commitFile(dirname, filename, contents, message string) error {
+	// Create file.
+	err := makeFile(dirname, filename, contents)
+	if err != nil {
+		return err
+	}
+
+	// Add file to git staging area.
+	cmd2 := exec.Command("git", "add", filename)
+	cmd2.Dir = dirname
+	err = cmd2.Run()
+	if err != nil {
+		return err
+	}
+
+	// Commit the file.
+	cmd3 := exec.Command("git", "commit", "-m", message)
+	cmd3.Dir = dirname
+	err = cmd3.Run()
+
+	return err
+}
+
+func createTag(dirname, tag string) error {
+	cmd := exec.Command("git", "tag", tag)
+	cmd.Dir = dirname
+	err := cmd.Run()
+
+	return err
+}
+
+func createAnnotatedTag(dirname, tag, annotation string) error {
+	cmd := exec.Command("git", "tag", "-a", tag, "-m", annotation)
+	cmd.Dir = dirname
+	err := cmd.Run()
+
+	return err
 }
